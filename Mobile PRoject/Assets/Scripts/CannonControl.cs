5a5719b188f7a8555fabaec38babc07da435c1b6
Shanee0 <38456645+Shanee0@users.noreply.github.com>
2018-04-18 22:08:57 +0100

    Added script to move the cannon and rotate it.

---
+++ Mobile PRoject/Assets/Scripts/CannonControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            RotateLeft();
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            RotateRight();
+        }
+    }
+
+    void RotateLeft()
+    {
+        transform.Rotate(Vector3.forward);
+    }
+
+    void RotateRight()
+    {
+        transform.Rotate(Vector3.back);
+    }
+}
